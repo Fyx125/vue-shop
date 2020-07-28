3681edb772ce9c35e918f326cfde71e4bfc176d6
yx666 <1251973330@qq.com>
2020-07-28 14:07:38 +0800

    end

--- src/main.js
+++ src/main.js
@@ -12,13 +12,22 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
-// 设置请求拦截器
+// 设置请求拦截器 request中展示进度条
 axios.interceptors.request.use(config => {
   // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+//  response中隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
